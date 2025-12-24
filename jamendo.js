2a02a47aa01c003dbb05692319813665eca78d48
Curtis Papa Ankomah Poku <curtispapaankomahpoku@gmail.com>
2025-12-24 02:26:43 +0000

    Fix environment variable access for JAMENDO_CLIENT_ID

--- jamendo.js
+++ jamendo.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 async function fetchJamendoTracks(search = '', order = 'popularity_week', limit = 50) {
-    const JAMENDO_CLIENT_ID = process.env.JAMENDO_CLIENT_ID || 'd32b0311';
+    const JAMENDO_CLIENT_ID = process.ENV.env.JAMENDO_CLIENT_ID || 'd32b0311';
     let url = `https://api.jamendo.com/v3.0/tracks/?client_id=${JAMENDO_CLIENT_ID}&format=jsonpretty&limit=${limit}`;
 
     if (search) {
